cff510b7f83144f630a909eda58c305de0d8c8b6
Simo <vedret.hrvanovic@gmail.com>
2018-08-29 11:45:16 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express=require('express');
+let morgan=require('morgan');
+
+let app = express();
+//Midleware
+app.use(morgan('dev'));
+
+app.get('/name' , function(req,res){
+    let age=10;
+    res.json('My name is funk so bravo, godina '+age );
+})
+
+app.listen(3000 , (err)=>{
+    if(err) throw err;
+    console.log('Server is running on port 3000')
+});
